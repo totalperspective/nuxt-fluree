e84b0455ff97447dddeb5bd7b66141991fa6670f
Bahul Neel Upadhyaya <bahul.upadhyaya@contxt.id>
2023-09-03 23:41:05 +0100

    formatting config

--- src/module.ts
+++ src/module.ts
@@ -5,15 +5,15 @@ export interface ModuleOptions {}
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
-    name: 'my-module',
-    configKey: 'myModule'
+    name: 'fluree',
+    configKey: 'fluree',
   },
   // Default configuration options of the Nuxt module
   defaults: {},
-  setup (options, nuxt) {
+  setup(options, nuxt) {
     const resolver = createResolver(import.meta.url)
 
     // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
     addPlugin(resolver.resolve('./runtime/plugin'))
-  }
+  },
 })
